6b679a1af128ab5f8de3f2cbee766c1042071f09
Thomas Bernard <thomas.bernard@diiage.org>
2022-09-08 10:10:23 +0200

    Create login page

--- InitManage/InitManage/App.xaml.cs
+++ InitManage/InitManage/App.xaml.cs
@@ -1,4 +1,6 @@
-﻿namespace InitManage;
+﻿using InitManage.Views.Pages;
+
+namespace InitManage;
 
 public partial class App : Application
 {
@@ -6,7 +8,7 @@ public partial class App : Application
 	{
 		InitializeComponent();
 
-		MainPage = new AppShell();
+		MainPage = new LoginPage();
 	}
 }
 
