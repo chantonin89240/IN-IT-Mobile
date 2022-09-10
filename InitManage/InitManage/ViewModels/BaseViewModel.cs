b99f707d80cadf98eb7eccfa85f93030f6d62d44
Thomas Bernard <thomas.bernard@diiage.org>
2022-09-10 11:22:24 +0200

    Add life cycle methods in base vm

--- InitManage/InitManage/ViewModels/BaseViewModel.cs
+++ InitManage/InitManage/ViewModels/BaseViewModel.cs
@@ -1,10 +1,53 @@
 ﻿using System;
+using Sharpnado.Tasks;
+
 namespace InitManage.ViewModels;
 
-public class BaseViewModel
+public class BaseViewModel : INavigatedAware, IInitializeAsync
 {
     public BaseViewModel(INavigationService navigationService)
     {
     }
+
+    #region LifeCycle
+
+    public Task InitializeAsync(INavigationParameters parameters)
+    {
+        return Task.FromResult(0);
+    }
+
+    public void OnNavigatedTo(INavigationParameters parameters)
+    {
+        TaskMonitor.Create(OnNavigatedToAsync(parameters),
+                           whenFaulted: t => {
+                               t.Exception.Handle(ex => {
+                                   Console.WriteLine($"OnNavigatedTo error : {ex.Message}");
+                                   return true;
+                               });
+                           });
+    }
+
+    protected virtual Task OnNavigatedToAsync(INavigationParameters parameters)
+    {
+        return Task.FromResult(0);
+    }
+
+
+    public void OnNavigatedFrom(INavigationParameters parameters)
+    {
+        TaskMonitor.Create(OnNavigatedFromAsync(parameters),
+                           whenFaulted: t => {
+                               t.Exception.Handle(ex => {
+                                   Console.WriteLine($"OnNavigatedFrom error : {ex.Message}");
+                                   return true;
+                               });
+                           });
+    }
+    protected virtual Task OnNavigatedFromAsync(INavigationParameters parameters)
+    {
+        return Task.FromResult(0);
+    }
+
+    #endregion
 }
 
--- InitManage/InitManage/ViewModels/Login/LoginViewModel.cs
+++ InitManage/InitManage/ViewModels/Login/LoginViewModel.cs
@@ -4,8 +4,23 @@ namespace InitManage.ViewModels.Login;
 
 public class LoginViewModel : BaseViewModel
 {
+
+    #region CTOR
     public LoginViewModel(INavigationService navigationService) : base(navigationService)
     {
     }
+    #endregion
+
+    #region Life cycle
+
+    #endregion
+
+    #region Properties
+
+    #endregion
+
+    #region Methods & Commands
+
+    #endregion
 }
 
