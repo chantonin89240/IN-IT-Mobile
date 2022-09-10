15c3c43241d76480e66950eba605f6e969ec1f2c
Thomas Bernard <thomas.bernard@diiage.org>
2022-09-10 11:39:35 +0200

    Bind properties and command of login page

--- InitManage/InitManage/ViewModels/Login/LoginViewModel.cs
+++ InitManage/InitManage/ViewModels/Login/LoginViewModel.cs
@@ -1,15 +1,16 @@
 ﻿using System;
+using System.Reactive;
+using InitManage.Views.Pages;
+using ReactiveUI;
 
 namespace InitManage.ViewModels.Login;
 
 public class LoginViewModel : BaseViewModel
 {
-
-    #region CTOR
     public LoginViewModel(INavigationService navigationService) : base(navigationService)
     {
+        LoginCommand = ReactiveCommand.CreateFromTask(OnLoginCommand);
     }
-    #endregion
 
     #region Life cycle
 
@@ -17,9 +18,35 @@ public class LoginViewModel : BaseViewModel
 
     #region Properties
 
+    #region Mail
+    private string _mail;
+    public string Mail
+    {
+        get => _mail;
+        set => this.RaiseAndSetIfChanged(ref _mail, value);
+    }
+    #endregion
+
+    #region Password
+    private string _password;
+    public string Password
+    {
+        get => _password;
+        set => this.RaiseAndSetIfChanged(ref _password, value);
+    }
+    #endregion
+
     #endregion
 
     #region Methods & Commands
+    #region OnLoginCommand
+
+    public ReactiveCommand<Unit, Unit> LoginCommand { get; }
+    private async Task OnLoginCommand()
+    {
+        Application.Current.MainPage = new MainTabbedPage();
+    }
+    #endregion
 
     #endregion
 }
