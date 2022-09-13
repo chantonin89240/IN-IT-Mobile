f0cb7865fed2388efc2c065390a9c03c9b74c172
Thomas Bernard <thomas.bernard@diiage.org>
2022-09-13 22:17:01 +0200

    Can toggle dark mode in app paramters

--- InitManage/InitManage/ViewModels/Setting/SettingsViewModel.cs
+++ InitManage/InitManage/ViewModels/Setting/SettingsViewModel.cs
@@ -18,9 +18,25 @@ public class SettingsViewModel : BaseViewModel
 
     #region Properties
 
+    #region DarkModeEnabled
+
+    private bool _darkModeEnabled;
+    public bool DarkModeEnabled
+    {
+        get => _darkModeEnabled;
+        set
+        {
+            SetDarkMode(value);
+            this.RaiseAndSetIfChanged(ref _darkModeEnabled, value);
+        }
+    }
+
+    #endregion
+
     #endregion
 
     #region Methods & Commands
+
     #region OnLogoutCommand
 
     public ReactiveCommand<Unit, Unit> LogoutCommand { get; private set; }
@@ -30,6 +46,19 @@ public class SettingsViewModel : BaseViewModel
     }
 
     #endregion
+
+    #region SetDarkMode
+
+    private void SetDarkMode(bool darkModeEnabled)
+    {
+        if (darkModeEnabled)
+            Application.Current.UserAppTheme = AppTheme.Dark;
+        else
+            Application.Current.UserAppTheme = AppTheme.Light;
+    }
+
+    #endregion
+
 
     #endregion
 }
