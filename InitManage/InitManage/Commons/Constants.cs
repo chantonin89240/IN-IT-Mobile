086ad16b83ce7aa0b1b0c21819243d5bd42eec5d
Thomas Bernard <thomas.bernard@diiage.org>
2022-09-16 14:41:44 +0200

    Repair android http calls

--- InitManage/InitManage/Commons/Constants.cs
+++ InitManage/InitManage/Commons/Constants.cs
@@ -5,7 +5,7 @@ public class Constants
 {
     public const string ResourceIdNavigationParameter = "resource";
 
-    public const string ApiBaseAdress = "http://localhost:3000/";
+    public const string ApiBaseAdress = "http://10.4.0.96:3000/";
     public const string ResourceEndPoint = "resource";
 }
 
