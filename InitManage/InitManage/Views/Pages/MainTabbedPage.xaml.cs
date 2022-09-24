c089c012f7b0de0b565f3fdbe9102ece409928c5
Thomas Bernard <thomas.bernard@diiage.org>
2022-09-24 20:47:35 +0200

    Disable create button if not admin

--- InitManage/InitManage/Views/Pages/MainTabbedPage.xaml.cs
+++ InitManage/InitManage/Views/Pages/MainTabbedPage.xaml.cs
@@ -1,17 +1,24 @@
-﻿using InitManage.ViewModels.Resource;
+﻿using InitManage.Helpers.Interfaces;
+using InitManage.ViewModels.Resource;
 using InitManage.ViewModels.Setting;
 
 namespace InitManage.Views.Pages;
 
 public partial class MainTabbedPage : TabbedPage
 {
+    private readonly IPreferenceHelper _preferenceHelper;
+
 	public MainTabbedPage()
 	{
+        /// MUST PUT THIS CODE HERE BECAUSE DI IS BROKEN PRISM TABBED PAGE
+
 		InitializeComponent();
 
         #if IOS
 		this.SelectedTabColor = (Color)Application.Current.Resources.MergedDictionaries.FirstOrDefault()["Primary"];
         #endif
+
+        _preferenceHelper = ContainerLocator.Container.Resolve(typeof(IPreferenceHelper)) as IPreferenceHelper;
 
         var resourcesPage = ContainerLocator.Container.Resolve<ResourcesPage>();
         resourcesPage.BindingContext = ContainerLocator.Container.Resolve<ResourcesViewModel>();
@@ -25,8 +32,11 @@ public partial class MainTabbedPage : TabbedPage
 		settingPage.BindingContext = ContainerLocator.Container.Resolve<SettingsViewModel>();
 		this.Children.Add(settingPage);
 
-        var createResourcePage = ContainerLocator.Container.Resolve<CreateResourcePage>();
-        createResourcePage.BindingContext = ContainerLocator.Container.Resolve<CreateResourceViewModel>();
-        this.Children.Add(createResourcePage);
+        if (_preferenceHelper?.IsAdmin ?? false)
+        {
+            var createResourcePage = ContainerLocator.Container.Resolve<CreateResourcePage>();
+            createResourcePage.BindingContext = ContainerLocator.Container.Resolve<CreateResourceViewModel>();
+            this.Children.Add(createResourcePage);
+        }
     }
 }
