7c05c545bb00c0f3ea533ab2712bfc7cee71d4b6
Thomas Bernard <thomas.bernard@diiage.org>
2022-09-24 17:27:38 +0200

    Create PreferenceHelper

--- InitManage/InitManage/Helpers/Interfaces/IPreferenceHelper.cs
+++ InitManage/InitManage/Helpers/Interfaces/IPreferenceHelper.cs
@@ -5,5 +5,7 @@ public interface IPreferenceHelper
 {
     bool IsAdmin { get; set; }
     string Username { get; set; }
+    TimeSpan TimeBeforeReceiveNotification { get; set; }
+    bool IsNotificationEnabled { get; set; }
 }
 
--- InitManage/InitManage/Helpers/PreferenceHelper.cs
+++ InitManage/InitManage/Helpers/PreferenceHelper.cs
@@ -5,16 +5,45 @@ namespace InitManage.Helpers;
 
 public class PreferenceHelper : IPreferenceHelper
 {
+
+    #region IsAdmin
     public bool IsAdmin
     {
         get => Preferences.Get(nameof(IsAdmin), false);
         set => Preferences.Set(nameof(IsAdmin), value);
     }
+    #endregion
 
+    #region IsNotificationEnabled
+    public bool IsNotificationEnabled
+    {
+        get => Preferences.Get(nameof(IsNotificationEnabled), false);
+        set => Preferences.Set(nameof(IsNotificationEnabled), value);
+    }
+    #endregion
+
+    #region Username
     public string Username
     {
         get => Preferences.Get(nameof(Username), string.Empty);
         set => Preferences.Set(nameof(Username), value);
     }
+    #endregion
+
+    #region TimeBeforeReceiveNotification
+    public TimeSpan TimeBeforeReceiveNotification
+    {
+        get
+        {
+            var minutes = Preferences.Get(nameof(TimeBeforeReceiveNotification), 10);
+            return TimeSpan.FromMinutes(minutes);
+        }
+        set
+        {
+            var minutes = value.Minutes;
+            Preferences.Set(nameof(TimeBeforeReceiveNotification), minutes);
+        }
+    }
+    #endregion
 }
 
