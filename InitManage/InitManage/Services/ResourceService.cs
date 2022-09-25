ac7c644e489b70a7f5a4ecb06fabfe5577e084a1
Thomas Bernard <thomas.bernard@diiage.org>
2022-09-25 09:55:19 +0200

    Get one resource

--- InitManage/InitManage/Services/ResourceService.cs
+++ InitManage/InitManage/Services/ResourceService.cs
@@ -18,9 +18,10 @@ public class ResourceService : IResourceService
         _httpService = httpService;
     }
 
-    public Task<IResource> GetResourceAsync(long id)
+    public async Task<IResource> GetResourceAsync(long id)
     {
-        throw new NotImplementedException();
+        var response = await _httpService.SendHttpRequest<ResourceDTODown>($"{Constants.ApiBaseAdress}{Constants.ResourceEndPoint}/{id}", HttpMethod.Get);
+        return response.Result;
     }
 
     public Task<IEnumerable<IBooking>> GetResourceBookingsAsync(long resourceId)
@@ -35,8 +36,8 @@ public class ResourceService : IResourceService
 
     public async Task<IEnumerable<IResource>> GetResourcesAsync()
     {
-        var resutl = await _httpService.SendHttpRequest<IEnumerable<ResourceDTODown>>($"{Constants.ApiBaseAdress}{Constants.ResourceEndPoint}", HttpMethod.Get);
-        return resutl.Result;
+        var response = await _httpService.SendHttpRequest<IEnumerable<ResourceDTODown>>($"{Constants.ApiBaseAdress}{Constants.ResourceEndPoint}", HttpMethod.Get);
+        return response.Result;
     }
 
     public async Task<bool> CreateResource(IResource resource)
