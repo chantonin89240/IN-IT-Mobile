2bb614548cd3555bac601a3ff0f6939faf752c00
Antonin CHATEIGNER <antonin.chateigner@diiage.org>
2022-10-06 20:49:31 +0200

    création du DTO et du service pour la création de booking

---
+++ InitManage/InitManage/Models/DTOs/BookingDTOUp.cs
@@ -0,0 +1,31 @@
+﻿using InitManage.Models.Interfaces;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InitManage.Models.DTOs
+{
+    public class BookingDTOUp : IBookingEntity
+    {
+        [JsonProperty("id")]
+        public long Id { get; set; }
+
+        [JsonProperty("id")]
+        public long UserId { get; set; }
+
+        [JsonProperty("id")]
+        public long ResourceId { get; set; }
+
+        [JsonProperty("id")]
+        public DateTime Start { get; set; }
+
+        [JsonProperty("id")]
+        public DateTime End { get; set; }
+
+        [JsonProperty("id")]
+        public int Capacity { get; set; }
+    }
+}
